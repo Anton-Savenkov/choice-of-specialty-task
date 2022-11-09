79ac203d306588fe140a1bfa8c9d1e7caf50ec55
Anton Savenkov <a_savi@icloud.com>
2022-11-09 22:58:05 +0300

    add input elements and funkcion screch

--- FinalTask/Program.cs
+++ FinalTask/Program.cs
@@ -1,2 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+Console.WriteLine("Введите елементы строки через побел");
+string strigElemens = Console.ReadLine();
+string[] inputUserStrigArr = strigElemens.Split(new Char[] {' '});
+
+for (int i = 0; i < inputUserStrigArr.Length; i++)
+{
+    Console.WriteLine(inputUserStrigArr[i]);
+}
+
+string[] resultArrayFull(string[] inputStrigArr)
+{
+    int arrayLeng = 0;
+    string[] resultArray = new string[arrayLeng];
+    for (int i = 0; i < inputStrigArr.Length; i++)
+    {
+        if (inputStrigArr[i].Length < 3)
+        {
+            arrayLeng++;
+            resultArray[i] = inputStrigArr[i];
+            Console.WriteLine(resultArray[i]);
+        }
+    }
+    return resultArray;
+}
+resultArrayFull(inputUserStrigArr);
+string[] resultArray = resultArrayFull(inputUserStrigArr);
+
